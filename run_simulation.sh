4e5dc4696587cdf1a82724d82e9eefd14214b06a
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2021-04-25 23:16:29 -0300

    Add obstacle avoidance

--- run_simulation.sh
+++ run_simulation.sh
@@ -2,4 +2,6 @@ cd ~/catkin_ws
 catkin_make -DCMAKE_EXPORT_COMPILE_COMMANDS=1 || exit 1
 rqt_console & 
 rviz -d /home/fede/catkin_ws/src/navigation_test/rviz/rviz.rviz & 
+roscore &
+sleep 3
 roslaunch navigation_test navigation_test.launch  
